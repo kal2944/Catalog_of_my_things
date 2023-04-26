594a9444a2105806cedae3679d59cfdf1c041489
kdan2944 <kdan2944gmail.com>
2023-04-26 04:58:02 +0300

    Add the properties and constructor

--- book.rb
+++ book.rb
@@ -1,4 +1,10 @@
 require_relative 'items'
 
 class Book < Item
- 
+    
+    def initialize(publisher, cover_state, id, publish_date, archived: false)
+        super(id, publish_date, archived: archived)
+        @publisher = publisher
+        @cover_state = cover_state
+      end
+    
